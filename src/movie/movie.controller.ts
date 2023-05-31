8b3b8724355c2cdcb145b8e326fcf9818d69499d
mustapha chatbi <chatbimdev@gmail.com>
2023-05-31 23:32:13 +0100

    Added swagger docs

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -5,35 +5,55 @@ import { Movie } from './movie.model';
 import { GetAuthUser } from './../auth/get-authenticated-user';
 import { User } from './../user/user.model';
 import { CreateMovieDto, UpdateMovieDto } from './movie.dto';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags, ApiBody, ApiParam } from '@nestjs/swagger';
 
+@ApiTags('movies')
 @Controller('movies')
 export class MovieController {
   constructor(private readonly movieService: MovieService) { }
 
   @Get()
+  @ApiOperation({ summary: 'Get all movies' })
+  @ApiResponse({ status: 200, description: 'The found records', type: Movie, isArray: true })
   async findAll(): Promise<Movie[]> {
     return this.movieService.findAll();
   }
 
   @Get('user/:userId')
+  @ApiOperation({ summary: 'Get all movies by user id' })
+  @ApiResponse({ status: 200, description: 'The found records', type: Movie, isArray: true })
+  @ApiParam({ name: 'userId', required: true, description: 'The user id' })
   async findByUser(@Param('userId') userId: string): Promise<Movie[]> {
     return this.movieService.findByUser(userId);
   }
 
   @UseGuards(AuthGuard('jwt'))
   @Post()
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Create a new movie' })
+  @ApiResponse({ status: 201, description: 'The record has been successfully created.', type: Movie })
+  @ApiBody({ type: CreateMovieDto })
   async create(@Body() movie: CreateMovieDto, @GetAuthUser() user: User): Promise<Movie> {
     return this.movieService.create(movie, user);
   }
 
   @UseGuards(AuthGuard('jwt'))
   @Put(':id')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Update a movie' })
+  @ApiResponse({ status: 200, description: 'The record has been successfully updated.', type: Movie })
+  @ApiParam({ name: 'id', required: true, description: 'The movie id' })
+  @ApiBody({ type: UpdateMovieDto })
   async update(@Param('id') id: string, @Body() updatedMovie: UpdateMovieDto, @GetAuthUser() user: User): Promise<Movie> {
     return this.movieService.update(id, updatedMovie, user);
   }
 
   @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Delete a movie' })
+  @ApiResponse({ status: 200, description: 'The record has been successfully deleted.', type: Movie })
+  @ApiParam({ name: 'id', required: true, description: 'The movie id' })
   async delete(@Param('id') id: string, @GetAuthUser() user: User): Promise<Movie> {
     return this.movieService.delete(id, user);
   }
--- src/movie/movie.dto.ts
+++ src/movie/movie.dto.ts
@@ -1,33 +1,62 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { IsDateString, IsEnum, IsString, IsUrl, Max, Min, MinLength, MaxLength } from 'class-validator';
 import { Category } from './movie.model';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateMovieDto {
+    @ApiProperty({ description: 'The title of the movie' })
     @IsString()
     @MinLength(2)
     @MaxLength(120)
     title: string;
 
+    @ApiProperty({ description: 'The description of the movie' })
     @IsString()
     @MinLength(20)
     @MaxLength(500)
     description: string;
 
+    @ApiProperty({ description: 'The release date of the movie' })
     @IsDateString()
     releaseDate: Date;
 
+    @ApiProperty({ description: 'The rating of the movie' })
     @Min(1)
     @Max(5)
     rating: number;
 
+    @ApiProperty({ enum: Category, description: 'The category of the movie' })
     @IsEnum(Category)
     category: Category;
 
+    @ApiProperty({ type: [String], description: 'The actors in the movie' })
     @IsString({ each: true })
     actors: string[];
 
+    @ApiProperty({ description: 'The poster URL of the movie' })
     @IsUrl()
     poster: string;
 }
 
-export class UpdateMovieDto extends PartialType(CreateMovieDto) { }
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {
+    @ApiPropertyOptional()
+    title?: string;
+
+    @ApiPropertyOptional()
+    description?: string;
+
+    @ApiPropertyOptional()
+    releaseDate?: Date;
+
+    @ApiPropertyOptional()
+    rating?: number;
+
+    @ApiPropertyOptional()
+    category?: Category;
+
+    @ApiPropertyOptional()
+    actors?: string[];
+
+    @ApiPropertyOptional()
+    poster?: string;
+}
